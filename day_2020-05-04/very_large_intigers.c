023afaabacfb5ffa83f57bdd7079a85ca29ee30c
Arkadiusz Czader <arkadiusz.czader232@gmail.com>
2020-05-04 15:08:30 +0200

    Add function that multiply VLI numbers

--- day_2020-05-04/very_large_intigers.c
+++ day_2020-05-04/very_large_intigers.c
@@ -19,19 +19,19 @@ vli* add_vli(vli *a, vli *b){
     vli *sum = (vli*)malloc(sizeof(vli));
 
     size_t len = (a->len>b->len)?a->len:b->len;
-    // Max length of result is len of longer number + 1
+    // Max length of result is length of longer number + 1
     sum->len = len + 1;
     sum->digits = (char*)malloc(sum->len*sizeof(char));
 
     // Adding numbers
-    for(int i = 0; i<len;i++){
+    for(int i = 0; i < len; i++){
         if(i < a->len)
             sum->digits[i] += a->digits[i];
         if(i < b->len)
             sum->digits[i] += b->digits[i];
 
 	// Carry to next column
-	sum->digits[i+1] = sum->digits[i] / 10;
+	sum->digits[i+1] += sum->digits[i] / 10;
 
 	// Keep only unit digit 
 	sum->digits[i] %= 10;
@@ -40,21 +40,46 @@ vli* add_vli(vli *a, vli *b){
     return sum;
 }
 
+vli* multiply_vli(vli *a, vli *b){
+    vli *result = (vli*)malloc(sizeof(vli));
+
+    // Max length of result is length of a + length of b
+    result->len = a->len + b->len;
+    result->digits = (char*)malloc(result->len*sizeof(char));
+
+    for(int i = 0; i < a->len; i++){
+        // Multiply b * a[i] and add it to result
+        for(int j = 0; j < b->len; j++){
+            result->digits[i+j] += a->digits[i] * b->digits[j];
+
+            // Carry to next column
+	    result->digits[i+j+1] += result->digits[i+j] / 10;
+
+	    // Keep only unit digit 
+	    result->digits[i+j] %= 10;
+        }
+    }
+
+    return result;
+}
 int main(){
     // Digits in REVERSED ORDER
-    char n1[] = {3, 3, 2, 3, 9, 2, 2, 9, 2, 9, 2};
+    char n1[] = {3, 9, 2, 3, 9, 2, 2, 9, 2, 9, 2};
     char n2[] = {9, 3, 2, 1, 3, 0, 0, 0, 3, 3};
 
     vli a = {n1, 11};
     vli b = {n2, 10};
 
-    vli* sum = add_vli(&a, &b);
+    vli *sum = add_vli(&a, &b);
+    vli *mul = multiply_vli(&a, &b);
 
     print_vli(&a);
     putchar('\n');
     print_vli(&b);
     putchar('\n');
     print_vli(sum);
+    putchar('\n');
+    print_vli(mul);
     putchar('\n');
 
     return 0;
